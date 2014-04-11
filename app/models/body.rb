9b12a18e03a31b62ba47e767e6ec71153044fe89
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-10 18:24:41 -0700

    I made a test work

--- app/models/body.rb
+++ app/models/body.rb
@@ -2,4 +2,5 @@ class Body
   include Mongoid::Document
   field :name, type: String
   field :years, type: Hash
+  validates :name, uniqueness: true
 end
--- spec/models/body_spec.rb
+++ spec/models/body_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Body do 
-  # let!(:body) { Body.create( name: "Tralfamador", years: 2000) }
+  let!(:body) { Body.create( name: "Tralfamador", years: {}) }
   it "should have unique names for bodies" do
-    body2 = Body.create(name: "Tralfamador")
+    body2 = Body.create(name: "Tralfamador", years: {})
     expect(body2).to_not be_valid
   end
 end
